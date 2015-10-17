cff2bf672fcf56167cc5a3829e263188dec0fbb8
Oki <enggalmandiri@gmail.com>
2015-10-18 04:26:47 +0700

    Init commit.

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+(function() {
+
+	'use strict';
+
+	var slideTotal = $('.sl-slide').length;
+
+	$('[init="bgImg"]').each(function() {
+		var $this = $(this),
+			img = new Image();
+
+		img.src = $(this).data('image');
+		img.onload = function() {
+			$this
+				.addClass('load')
+				.css('backgroundImage', 'url(' + img.src + ')');
+		}
+	});
+
+	$('[action="nav"] > span').on('click', function() {
+		var $this = $(this);
+		
+		$this.parents('nav').find('span > div').show();
+		$this.find('> div').hide();
+	});
+
+	$('[action="answer"] a').on('click', function() {
+		var nextSlide = $(this).parents('.sl-slide').index();
+		if(++nextSlide + 1 > slideTotal) {
+			nextSlide = 0;
+		}
+
+		$(this)
+			.parents('ul').find('a').removeClass('selected').end().end()
+			.addClass('selected');
+		
+		$('[action="nav"] span:eq(' + nextSlide + ')').trigger('click');
+	});
+
+})();
